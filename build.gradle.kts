a3936c4694ac49845ff86144e13487e231e46a27
Space Nerd <florianpospiech77@gmail.com>
2023-02-19 19:51:46 +0100

    Build basic Structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("xyz.jpenilla.run-paper") version "2.0.0"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
+
 }
 
 group = "net.onelitefeather"
@@ -7,19 +11,35 @@ version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/")
+    maven { url = uri("https://repo.papermc.io/repository/maven-public/") }
 }
 
 dependencies {
     // Paper
-    compileOnly(libs.paper)
+    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
+    implementation("cloud.commandframework:cloud-annotations:1.7.1")
+    annotationProcessor("cloud.commandframework:cloud-annotations:1.7.1")
+    library("com.google.code.gson", "gson", "2.8.7") // All platforms
+    bukkitLibrary("com.google.code.gson", "gson", "2.8.7") // Bukkit only
+    implementation("net.kyori:adventure-api:4.12.0")
 }
+
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
 
+bukkit {
+    main = "net.onelitefeather.butterfly.Butterfly"
+    apiVersion = "1.13"
+}
+
+
 tasks {
+    runServer {
+        minecraftVersion("1.19.3")
+    }
+
     compileJava {
         options.release.set(17)
         options.encoding = "UTF-8"
---
+++ src/main/java/net/onelitefeather/butterfly/Butterfly.java
@@ -0,0 +1,4 @@
+package net.onelitefeather.butterfly;
+
+public class Butterfly {
+}
