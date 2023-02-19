987ab96022257e9d3d9972e2097bdf6576cb3dfd
Space Nerd <florianpospiech77@gmail.com>
2023-02-19 20:35:38 +0100

    Set up basic Command Structure

--- src/main/java/net/onelitefeather/butterfly/Butterfly.java
+++ src/main/java/net/onelitefeather/butterfly/Butterfly.java
@@ -1,4 +1,38 @@
 package net.onelitefeather.butterfly;
 
-public class Butterfly {
+import cloud.commandframework.annotations.AnnotationParser;
+import cloud.commandframework.arguments.parser.ParserParameters;
+import cloud.commandframework.arguments.parser.StandardParameters;
+import cloud.commandframework.meta.CommandMeta;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+import cloud.commandframework.paper.PaperCommandManager;
+
+import java.util.function.Function;
+
+public class Butterfly extends JavaPlugin {
+
+
+    private final PaperCommandManager<CommandSender> paperCommandManager;
+    private final AnnotationParser<CommandSender> annotationParser;
+
+    final Function<ParserParameters, CommandMeta> commandMetaFunction = p ->
+            CommandMeta.simple().with(CommandMeta.DESCRIPTION, p.get(StandardParameters.DESCRIPTION, "No description")).build();
+
+    public Butterfly(PaperCommandManager<CommandSender> paperCommandManager) {
+        this.paperCommandManager = paperCommandManager;
+        this.annotationParser = new AnnotationParser<>(paperCommandManager, CommandSender.class, commandMetaFunction);
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+
+
+    @Override
+    public void onDisable() {
+
+    }
 }
