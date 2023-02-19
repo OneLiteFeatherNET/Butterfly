b5fd827ecb73896e4a156433c7c64cd5b9366eb4
Space Nerd <florianpospiech77@gmail.com>
2023-02-19 20:56:12 +0100

    Remove Player Join and Leave Message

---
+++ src/main/java/net/onelitefeather/butterfly/listeners/PlayerLeaveListener.java
@@ -0,0 +1,12 @@
+package net.onelitefeather.butterfly.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerLeaveListener implements Listener {
+    @EventHandler
+    public void playerLeave(PlayerQuitEvent event){
+        event.quitMessage(null);
+    }
+}
