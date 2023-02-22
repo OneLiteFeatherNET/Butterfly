7b7d7eace8e828b1285cfa3587a7d6d59df0ac1b
Phillipp Glanz <phillipp@kallmeier-glanz.de>
2023-02-22 13:14:45 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,6 +67,9 @@ tasks {
     getByName<org.sonarqube.gradle.SonarTask>("sonar") {
         dependsOn(rootProject.tasks.test)
     }
+    shadowJar {
+        archiveFileName.set("${rootProject.name}.${archiveExtension.getOrElse("jar")}")
+    }
 }
 
 sonarqube {
