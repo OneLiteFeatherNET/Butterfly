f6adafe230f959f753b24e5537bc64419c09798b
Phillipp Glanz <p.glanz@madfix.me>
2023-08-17 19:13:53 +0200

    Add minestom

---
+++ minestom/src/main/java/net/onelitefeather/butterfly/minestom/Butterfly.java
@@ -0,0 +1,16 @@
+package net.onelitefeather.butterfly.minestom;
+
+import net.minestom.server.extensions.Extension;
+import net.onelitefeather.butterfly.api.LuckPermsAPI;
+
+public class Butterfly extends Extension {
+    @Override
+    public void initialize() {
+        LuckPermsAPI.luckPermsAPI().subscribeEvents();
+    }
+
+    @Override
+    public void terminate() {
+        LuckPermsAPI.luckPermsAPI().unsubscribeEvents();
+    }
+}
