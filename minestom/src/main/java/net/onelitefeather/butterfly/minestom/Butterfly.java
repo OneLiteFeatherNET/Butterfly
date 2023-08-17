cf02efb6f0e5a5712166ff1ec57e01c06621289a
TheMeinerLP <p.glanz@madfix.me>
2023-08-17 21:26:00 +0200

    Implement chat and join.

--- minestom/src/main/java/net/onelitefeather/butterfly/minestom/Butterfly.java
+++ minestom/src/main/java/net/onelitefeather/butterfly/minestom/Butterfly.java
@@ -1,5 +1,11 @@
 package net.onelitefeather.butterfly.minestom;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.player.PlayerChatEvent;
+import net.minestom.server.event.player.PlayerLoginEvent;
 import net.minestom.server.extensions.Extension;
 import net.onelitefeather.butterfly.api.LuckPermsAPI;
 
@@ -8,6 +14,23 @@ public class Butterfly extends Extension {
     public void initialize() {
         LuckPermsAPI.setLuckPermsService(new MinestomLuckPermsService());
         LuckPermsAPI.luckPermsAPI().subscribeEvents();
+        MinecraftServer.getGlobalEventHandler().addListener(PlayerChatEvent.class, this::playerChat);
+        MinecraftServer.getGlobalEventHandler().addListener(PlayerLoginEvent.class, this::playerLogin);
+    }
+
+    private void playerLogin(PlayerLoginEvent playerLoginEvent) {
+        Player player = playerLoginEvent.getPlayer();
+        String displayName = LuckPermsAPI.luckPermsAPI().getGroupPrefix(LuckPermsAPI.luckPermsAPI().getPrimaryGroup(player.getUuid())) + player.getUsername();
+        player.setDisplayName(MiniMessage.miniMessage().deserialize(displayName));
+    }
+
+    private void playerChat(PlayerChatEvent playerChatEvent) {
+
+        playerChatEvent.setChatFormat(playerChatEvent1 -> Component.text()
+                .append(playerChatEvent1.getPlayer().getDisplayName())
+                .append(Component.text(": "))
+                .append(MiniMessage.miniMessage().deserialize(playerChatEvent1.getMessage()))
+                .build());
     }
 
     @Override
