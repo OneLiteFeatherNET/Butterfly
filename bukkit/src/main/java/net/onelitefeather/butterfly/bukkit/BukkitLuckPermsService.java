0bb9b503ba15a4d0c643f43731722cabcacf4b99
TheMeinerLP <p.glanz@madfix.me>
2023-10-10 20:22:51 +0200

    Add update logic for tab for teams and players.

--- bukkit/src/main/java/net/onelitefeather/butterfly/bukkit/BukkitLuckPermsService.java
+++ bukkit/src/main/java/net/onelitefeather/butterfly/bukkit/BukkitLuckPermsService.java
@@ -8,6 +8,9 @@ import net.onelitefeather.butterfly.api.LuckPermsAPI;
 import net.onelitefeather.butterfly.api.LuckPermsService;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Team;
+
+import java.util.Optional;
 
 public final class BukkitLuckPermsService implements LuckPermsService {
 
@@ -19,7 +22,29 @@ public final class BukkitLuckPermsService implements LuckPermsService {
     @Override
     public void setDisplayName(User user) {
         Player player = Bukkit.getPlayer(user.getUniqueId());
-        if (player != null)
+        if (player != null) {
             player.displayName(MiniMessage.miniMessage().deserialize(LuckPermsAPI.luckPermsAPI().getGroupPrefix(LuckPermsAPI.luckPermsAPI().getPrimaryGroup(player.getUniqueId())) + player.getName()));
+            var group = LuckPermsAPI.luckPermsAPI().getPrimaryGroup(player.getUniqueId());
+            String displayName = LuckPermsAPI.luckPermsAPI().getGroupPrefix(group) + player.getName();
+            player.displayName(MiniMessage.miniMessage().deserialize(displayName));
+            player.playerListName(MiniMessage.miniMessage().deserialize(displayName));
+            var mainScoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+            Optional.ofNullable(mainScoreboard.getPlayerTeam(player)).ifPresent(team -> team.removePlayer(player));
+            var weight = group.getWeight().orElse(9999);
+            var teamName = String.format("%04d", weight) + group.getName();
+            var team = mainScoreboard.getTeam(teamName);
+            if (team == null) {
+                team = mainScoreboard.registerNewTeam(teamName);
+                team.prefix(MiniMessage.miniMessage().deserialize(LuckPermsAPI.luckPermsAPI().getGroupPrefix(group)));
+                team.displayName(MiniMessage.miniMessage().deserialize(LuckPermsAPI.luckPermsAPI().getGroupPrefix(group)));
+                team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.ALWAYS);
+                team.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
+            }
+            if (!team.hasPlayer(player)) {
+                team.removePlayer(player);
+            }
+
+            team.addPlayer(player);
+        }
     }
 }
