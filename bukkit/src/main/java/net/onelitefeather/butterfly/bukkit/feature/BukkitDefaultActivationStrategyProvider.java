e8a014df9f72c6d5af0aaba180cd93625f57d3d3
Phillipp Glanz <p.glanz@madfix.me>
2024-01-17 14:23:14 +0100

    [butterfly#8] Handle some pr feedback

--- bukkit/src/main/java/net/onelitefeather/butterfly/bukkit/feature/BukkitDefaultActivationStrategyProvider.java
+++ bukkit/src/main/java/net/onelitefeather/butterfly/bukkit/feature/BukkitDefaultActivationStrategyProvider.java
@@ -4,7 +4,7 @@ import java.util.ServiceLoader;
 import org.togglz.core.activation.DefaultActivationStrategyProvider;
 import org.togglz.core.spi.ActivationStrategy;
 
-public class BukkitDefaultActivationStrategyProvider extends DefaultActivationStrategyProvider {
+public final class BukkitDefaultActivationStrategyProvider extends DefaultActivationStrategyProvider {
 
     public BukkitDefaultActivationStrategyProvider(ClassLoader classLoader) {
         for (ActivationStrategy activationStrategy : ServiceLoader.load(ActivationStrategy.class, classLoader)) {
--- minestom/src/main/java/net/onelitefeather/butterfly/minestom/feature/MinestomDefaultActivationStrategyProvider.java
+++ minestom/src/main/java/net/onelitefeather/butterfly/minestom/feature/MinestomDefaultActivationStrategyProvider.java
@@ -4,7 +4,7 @@ import java.util.ServiceLoader;
 import org.togglz.core.activation.DefaultActivationStrategyProvider;
 import org.togglz.core.spi.ActivationStrategy;
 
-public class MinestomDefaultActivationStrategyProvider extends DefaultActivationStrategyProvider {
+public final class MinestomDefaultActivationStrategyProvider extends DefaultActivationStrategyProvider {
 
     public MinestomDefaultActivationStrategyProvider(ClassLoader classLoader) {
         for (ActivationStrategy activationStrategy : ServiceLoader.load(ActivationStrategy.class, classLoader)) {
