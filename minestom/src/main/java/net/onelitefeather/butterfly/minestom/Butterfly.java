287ca97455e51ff4ddd329bbedff088fd6a9a779
Phillipp Glanz <p.glanz@madfix.me>
2024-01-17 10:16:45 +0100

    Implement togglz feature flags support

--- minestom/src/main/java/net/onelitefeather/butterfly/minestom/Butterfly.java
+++ minestom/src/main/java/net/onelitefeather/butterfly/minestom/Butterfly.java
@@ -6,28 +6,44 @@ import net.minestom.server.MinecraftServer;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.player.PlayerChatEvent;
 import net.minestom.server.event.player.PlayerLoginEvent;
+import net.minestom.server.event.player.PlayerSpawnEvent;
 import net.minestom.server.extensions.Extension;
 import net.onelitefeather.butterfly.api.LuckPermsAPI;
+import net.onelitefeather.butterfly.minestom.feature.ButterflyFeatures;
+import net.onelitefeather.butterfly.minestom.feature.MinestomDefaultActivationStrategyProvider;
+import org.togglz.core.activation.DefaultActivationStrategyProvider;
+import org.togglz.core.activation.SystemPropertyActivationStrategy;
+import org.togglz.core.context.StaticFeatureManagerProvider;
+import org.togglz.core.manager.FeatureManagerBuilder;
 
 public class Butterfly extends Extension {
+
+
     @Override
     public void initialize() {
+        Thread.currentThread().setContextClassLoader(getOrigin().getClassLoader());
+        StaticFeatureManagerProvider.setFeatureManager(FeatureManagerBuilder.begin()
+                .activationStrategyProvider(new MinestomDefaultActivationStrategyProvider(getOrigin().getClassLoader()))
+                .activationStrategy(new SystemPropertyActivationStrategy())
+                .featureEnum(ButterflyFeatures.class).build());
         LuckPermsAPI.setLuckPermsService(new MinestomLuckPermsService());
         LuckPermsAPI.luckPermsAPI().subscribeEvents();
+
         MinecraftServer.getGlobalEventHandler().addListener(PlayerChatEvent.class, this::playerChat);
-        MinecraftServer.getGlobalEventHandler().addListener(PlayerLoginEvent.class, this::playerLogin);
+        MinecraftServer.getGlobalEventHandler().addListener(PlayerSpawnEvent.class, this::playerSpawn);
     }
 
-    private void playerLogin(PlayerLoginEvent playerLoginEvent) {
-        Player player = playerLoginEvent.getPlayer();
-        String displayName = LuckPermsAPI.luckPermsAPI().getGroupPrefix(LuckPermsAPI.luckPermsAPI().getPrimaryGroup(player.getUuid())) + player.getUsername();
-        player.setDisplayName(MiniMessage.miniMessage().deserialize(displayName));
+    private void playerSpawn(PlayerSpawnEvent playerSpawnEvent) {
+        Player player = playerSpawnEvent.getPlayer();
+        LuckPermsAPI.luckPermsAPI().setDisplayName(LuckPermsAPI.luckPermsAPI().getUser(player.getUuid()));
     }
 
     private void playerChat(PlayerChatEvent playerChatEvent) {
-
+        Player player = playerChatEvent.getPlayer();
+        var group = LuckPermsAPI.luckPermsAPI().getPrimaryGroup(player.getUuid());
+        String displayName = LuckPermsAPI.luckPermsAPI().getGroupPrefix(group) + player.getUsername();
         playerChatEvent.setChatFormat(playerChatEvent1 -> Component.text()
-                .append(playerChatEvent1.getPlayer().getDisplayName())
+                .append(MiniMessage.miniMessage().deserialize(displayName))
                 .append(Component.text(": "))
                 .append(MiniMessage.miniMessage().deserialize(playerChatEvent1.getMessage()))
                 .build());
