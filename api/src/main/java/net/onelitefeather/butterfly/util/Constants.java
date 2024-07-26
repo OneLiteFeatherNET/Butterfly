dcf28f13f6c9683e46f88c53c21f90f324018972
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-07-26 21:15:55 +0200

    Add file

---
+++ api/src/main/java/net/onelitefeather/butterfly/util/Constants.java
@@ -0,0 +1,10 @@
+package net.onelitefeather.butterfly.util;
+
+public final class Constants {
+
+    private Constants() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String TEAM_COLOR_PERMISSION = "color.%s.%s";
+}
