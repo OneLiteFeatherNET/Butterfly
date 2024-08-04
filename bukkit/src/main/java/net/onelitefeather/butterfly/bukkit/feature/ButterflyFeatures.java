e14e9044e06775fc68bad7dcb43966579829c4f2
TheMeinerLP <p.glanz@madfix.me>
2024-08-04 13:20:48 +0200

    [butterfly#11] Implement thread helper and better way of togglz also fix scoreboard for members

--- bukkit/src/main/java/net/onelitefeather/butterfly/bukkit/feature/ButterflyFeatures.java
+++ bukkit/src/main/java/net/onelitefeather/butterfly/bukkit/feature/ButterflyFeatures.java
@@ -1,14 +1,18 @@
 package net.onelitefeather.butterfly.bukkit.feature;
 
+import net.onelitefeather.butterfly.bukkit.utils.ThreadHelper;
 import org.togglz.core.Feature;
+import org.togglz.core.annotation.EnabledByDefault;
 import org.togglz.core.annotation.Label;
+import org.togglz.core.context.FeatureContext;
 
-public enum ButterflyFeatures implements Feature {
+public enum ButterflyFeatures implements Feature, ThreadHelper {
+    @EnabledByDefault
     @Label("Team Collision")
     TEAM_COLLISION
     ;
 
     public boolean isActive() {
-        return BukkitFeatureContext.getFeatureManager().isActive(this);
+        return syncThreadForServiceLoader(() -> FeatureContext.getFeatureManager().isActive(this));
     }
 }
