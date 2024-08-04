458da1e4b15398cf0ddc4a487edb21daab31c860
TheMeinerLP <p.glanz@madfix.me>
2024-08-04 13:20:33 +0200

    [butterfly#11] Implement thread helper and better way of togglz

--- minestom/src/main/java/net/onelitefeather/butterfly/minestom/feature/ButterflyFeatures.java
+++ minestom/src/main/java/net/onelitefeather/butterfly/minestom/feature/ButterflyFeatures.java
@@ -2,13 +2,14 @@ package net.onelitefeather.butterfly.minestom.feature;
 
 import org.togglz.core.Feature;
 import org.togglz.core.annotation.Label;
+import org.togglz.core.context.FeatureContext;
 
-public enum ButterflyFeatures implements Feature {
+public enum ButterflyFeatures implements Feature, ThreadHelper {
     @Label("Team Collision")
     TEAM_COLLISION
     ;
 
     public boolean isActive() {
-        return MinestomFeatureContext.getFeatureManager().isActive(this);
+        return syncThreadForServiceLoader(() -> FeatureContext.getFeatureManager().isActive(this));
     }
 }
