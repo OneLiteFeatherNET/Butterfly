2122d3a0ffa6fb223129ae68e7b096b3433c40a7
TheMeinerLP <p.glanz@madfix.me>
2024-08-04 15:52:25 +0200

    [butterfly#13] Update to java 21 for bukkit

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -23,9 +23,9 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 
