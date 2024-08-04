7b191a7f4a88cf474fabac4ae60d444ecc20dbe9
TheMeinerLP <p.glanz@madfix.me>
2024-08-04 18:21:04 +0200

    [butterfly#13] Implement two commands to refresh in-game teams for bukkit and fix login sequence

--- bukkit/src/main/java/net/onelitefeather/butterfly/bukkit/Butterfly.java
+++ bukkit/src/main/java/net/onelitefeather/butterfly/bukkit/Butterfly.java
@@ -1,6 +1,8 @@
 package net.onelitefeather.butterfly.bukkit;
 
 import net.onelitefeather.butterfly.api.LuckPermsAPI;
+import net.onelitefeather.butterfly.bukkit.command.ClearTeamsCommand;
+import net.onelitefeather.butterfly.bukkit.command.UpdateTeamsCommand;
 import net.onelitefeather.butterfly.bukkit.listener.PlayerListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,6 +13,8 @@ public class Butterfly extends JavaPlugin {
         LuckPermsAPI.setLuckPermsService(new BukkitLuckPermsService());
         LuckPermsAPI.luckPermsAPI().subscribeEvents();
         getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+        getServer().getCommandMap().register("clearteams", new ClearTeamsCommand());
+        getServer().getCommandMap().register("updateteams", new UpdateTeamsCommand());
     }
 
     @Override
