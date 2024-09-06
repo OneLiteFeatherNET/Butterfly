3233237927e93b204b48950293c46c213b0d8d12
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-09-06 20:50:41 +0200

    Add method

--- api/src/main/java/net/onelitefeather/butterfly/api/LuckPermsAPI.java
+++ api/src/main/java/net/onelitefeather/butterfly/api/LuckPermsAPI.java
@@ -1,14 +1,17 @@
 package net.onelitefeather.butterfly.api;
 
+import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.group.Group;
+import net.luckperms.api.model.user.User;
 import net.luckperms.api.query.QueryOptions;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
  * Luckperms API for internal access
  */
-public sealed interface LuckPermsAPI permits LuckPermsAPIImplementation{
+public sealed interface LuckPermsAPI permits LuckPermsAPIImplementation {
 
     static LuckPermsAPI luckPermsAPI() {
         return LuckPermsAPIImplementation.INSTANCE;
@@ -37,5 +40,25 @@ public sealed interface LuckPermsAPI permits LuckPermsAPIImplementation{
         }
         throw new NullPointerException("Group prefix is null");
     }
+
+    default User getUser(UUID uuid) {
+        return LuckPermsProvider.get().getUserManager().getUser(uuid);
+    }
+
+    default int getGroupSortId(Group group) {
+
+        List<Group> sortedGroups = LuckPermsAPIImplementation.LUCK_PERMS.getGroupManager()
+                .getLoadedGroups().stream().sorted(LuckPermsAPIImplementation.GROUP_COMPARATOR).toList();
+
+        return sortedGroups.indexOf(group) + 1;
+    }
+
+    default void setDisplayName(User user) {
+        LuckPermsAPIImplementation.LUCK_PERMS_SERVICE.setDisplayName(user);
+    }
+
+    void subscribeEvents();
+
+    void unsubscribeEvents();
 
 }
