505875ecaba407a011ba6d3d8904cbf8a94d5a56
Phillipp Glanz <p.glanz@madfix.me>
2025-03-09 13:05:37 +0100

    Update repository URLs in build configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,26 +11,6 @@ pluginManagement {
 
 
 dependencyResolutionManagement {
-    if (System.getenv("CI") != null) {
-        repositoriesMode = RepositoriesMode.PREFER_SETTINGS
-        repositories {
-            maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            maven("https://repo.htl-md.schule/repository/Gitlab-Runner/")
-            maven {
-                val groupdId = 28 // Gitlab Group
-                val ciApiv4Url = System.getenv("CI_API_V4_URL")
-                url = uri("$ciApiv4Url/groups/$groupdId/-/packages/maven")
-                name = "GitLab"
-                credentials(HttpHeaderCredentials::class.java) {
-                    name = "Job-Token"
-                    value = System.getenv("CI_JOB_TOKEN")
-                }
-                authentication {
-                    create<HttpHeaderAuthentication>("header")
-                }
-            }
-        }
-    }
     versionCatalogs {
         create("libs") {
             version("paper", "1.20.4-R0.1-SNAPSHOT")
