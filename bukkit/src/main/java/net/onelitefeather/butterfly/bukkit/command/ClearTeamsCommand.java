5dd0887b0f290265f2a32807e464f879452abd9c
TheMeinerLP <github@themeinerlp.dev>
2026-09-13 19:29:12 +0200

    chore(debug): add logging to ClearTeamsCommand for team clearance actions

--- bukkit/src/main/java/net/onelitefeather/butterfly/bukkit/command/ClearTeamsCommand.java
+++ bukkit/src/main/java/net/onelitefeather/butterfly/bukkit/command/ClearTeamsCommand.java
@@ -6,8 +6,16 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 
 public class ClearTeamsCommand extends Command {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClearTeamsCommand.class);
+    private static final Marker SENSITIVITY = MarkerFactory.getMarker("SENSITIVITY");
+
     public ClearTeamsCommand() {
         super("clearteams");
         setPermission("butterfly.clearteams");
@@ -18,6 +26,8 @@ public class ClearTeamsCommand extends Command {
         Scoreboard mainScoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
         mainScoreboard.getTeams().forEach(Team::unregister);
         commandSender.sendRichMessage("<red>Teams have been removed");
+        LOGGER.info(SENSITIVITY, "Cleared all teams");
+        LOGGER.debug(mainScoreboard.toString());
         return true;
     }
 }
